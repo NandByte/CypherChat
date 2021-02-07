47eeb9c90ee02fa7375025906729a832944f0d1f
arctronic <shihabsikder98@gmail.com>
2021-02-07 14:34:20 +0600

    Annon setup successfull

--- app/src/main/java/com/nandbyte/cypherchat/MainActivity.java
+++ app/src/main/java/com/nandbyte/cypherchat/MainActivity.java
@@ -1,14 +1,64 @@
 package com.nandbyte.cypherchat;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.Toolbar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    private Toolbar mToolBar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
+        mToolBar = (Toolbar) findViewById(R.id.main_page_toolbar);
+        setSupportActionBar(mToolBar);
+        getSupportActionBar().setTitle("CypherChat");
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser == null){
+            sentTostart();
+        }
+    }
+
+    private void sentTostart() {
+        Intent startIntent = new Intent(MainActivity.this,LandingPage.class);
+        startActivity(startIntent);
+        finish();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.mainmenu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        super.onContextItemSelected(item);
+        if(item.getItemId() == R.id.main_logout){
+            FirebaseAuth.getInstance().signOut();
+            sentTostart();
+        }
+        return true;
     }
 }
